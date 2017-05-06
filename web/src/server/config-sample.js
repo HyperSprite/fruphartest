1fae27d318f163a4f54afbfdf5cfd85a9c6a5f8d
Christopher Dunavan <github@hypersprite.com>
2017-05-05 20:11:08 -0700

    update sample config to match real config

--- web/src/server/config-sample.js
+++ web/src/server/config-sample.js
@@ -1,18 +1,36 @@
-// Rename this file to config.js and randomize the secret string
+// Rename this file to config.js and randomize the secret string add your own api keys
 
-module.exports = {
-  secret: 'some-secret-string-that-should-not-be-commited',
-  rootURL: '', // Used for server
-  siteURL: 'http://localhost:3080', // Used for client auth callback
-  port: 3080,
-  public: 'public',
+const hlpr = require('./lib/helpers');
+
+const config = {
   mongoconnect: {
-    dev: 'mongodb://localhost:araceathlete-dev/araceathlete-dev',
-    prod: 'mongodb://localhost:araceathlete-prod/araceathlete-prod',
+    dev: 'mongodb://localhost/frugalpharma-dev',
+    prod: 'YOUR_OWN_PROD_CONNECTION',
   },
-  stravaLogin: {
-    clientID: 'STRAVA_CLIENT_ID',
-    clientSecret: 'STRAVA_CLIENT_SECRET',
-    code: 'STRAVA_CLIENT_CODE',
-  },
+  AUTH_SECRET: 'YOUR_AUTH_SECRET',  // Needed for bcrypt
+  CERT: 'false', // For production certs example, Heroku CERT = true
+  GOOGLE_MAPS: 'GOOGLE_MAPS_KEY',
+  LOGGING: 'false', // turns on hlpr.consLog can be used in prod if needed.
+  OPEN_WEATHER_MAP: 'OPEN_WEATHER_MAP_KEY', // Not currently needed
+  PORT: 4080,
+  ROOT_URL: 'http://localhost:4080',
+  SITE_PUBLIC: 'public', // static resources
+  SITE_URL: '',
+};
+
+const localMongoURI = !hlpr.isProd() ?
+  config.mongoconnect.dev :
+  config.mongoconnect.prod;
+
+exports.loadConfig = () => {
+  process.env.AUTH_SECRET = process.env.AUTH_SECRET || config.AUTH_SECRET;
+  process.env.CERT = process.env.CERT || config.CERT;
+  process.env.GOOGLE_MAPS = process.env.GOOGLE_MAPS || config.GOOGLE_MAPS;
+  process.env.MONGODB_URI = process.env.MONGODB_URI || localMongoURI;
+  process.env.LOGGING = process.env.LOGGING || config.LOGGING;
+  process.env.OPEN_WEATHER_MAP = process.env.OPEN_WEATHER_MAP || config.OPEN_WEATHER_MAP;
+  process.env.PORT = process.env.PORT || config.PORT;
+  process.env.ROOT_URL = process.env.ROOT_URL || config.ROOT_URL;
+  process.env.SITE_PUBLIC = process.env.SITE_PUBLIC || config.SITE_PUBLIC;
+  process.env.SITE_URL = process.env.SITE_URL || config.SITE_URL;
 };
